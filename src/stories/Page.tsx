ea8bc4bd2783113ededab73ef765e6af95c1595f
Jan Bayer <jan.bayer@auctores.de>
2025-08-28 11:17:36 +0200

    Add form to reprodution

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import Link from 'next/link';
+import Form from "next/form";
 
 export const Page: React.FC = () => {
-  return (
+  return (<>
     <Link href="/">Test-Link</Link>
+    <Form action="/">
+      <input type="text" name="test" id="test" />
+      <button type="submit">Submit</button>
+    </Form>
+  </>
   );
 };
